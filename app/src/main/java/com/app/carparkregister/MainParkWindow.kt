8cda15308687db90e4cbfdd2d83ca3417ac7df7c
Šípka Matej <matej.sipka@ixperta.com>
2018-05-01 21:05:18 +0200

    logout + email label in login screen

--- app/src/main/java/com/app/carparkregister/MainParkWindow.kt
+++ app/src/main/java/com/app/carparkregister/MainParkWindow.kt
@@ -1,9 +1,13 @@
 package com.app.carparkregister
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 
 import android.os.Bundle
 import android.support.v4.view.ViewPager
+import android.view.Menu
+import android.view.MenuItem
+import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_main_park_window.*
 
 class MainParkWindow : AppCompatActivity() {
@@ -21,11 +25,34 @@ class MainParkWindow : AppCompatActivity() {
         main_tab_layout.setupWithViewPager(viewPager)
     }
 
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_main_park_window, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        var itemId = item?.itemId
+            when(itemId){
+                R.id.logout -> {
+                    var instance = FirebaseAuth.getInstance()
+                    instance.signOut()
+                    val intent = Intent(this, LoginScreen::class.java)
+                    startActivity(intent)
+                    finish()
+                }
+                R.id.garage -> {
+
+                }
+            }
+
+
+        return super.onOptionsItemSelected(item)
+    }
 
     fun setupViewPager(viewPager:ViewPager){
 
         var adapter:SectionsPageAdapter = SectionsPageAdapter(supportFragmentManager)
-        adapter.addFragment(TabFragmentOne(), "GARAZ")
+        adapter.addFragment(TabFragmentOne(), "GARÁŽ")
         adapter.addFragment(TabFragmentTwo(), "MEDIA HALL")
         adapter.addFragment(TabFragmentThree(), "JIP")
         viewPager.adapter = adapter
